396f2e0fd97efc7eabd3bea17c5e6a1e58ece5f0
Ryoeik <s.hoang2811@gmail.com>
2026-03-02 11:34:50 +0100

    useRef Import Fix HalleDetail

--- src/pages/HalleDetail.jsx
+++ src/pages/HalleDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
 import { supabase } from '../supabase'
 import TickButton from '../components/TickButton'
